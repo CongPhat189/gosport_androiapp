04a385bf92297cb4adc3e230a099b112371c7036
Công Phát Trần <tpn18092004@gmail.com>
2026-03-21 22:46:21 +0700

    merge Thang

--- GoSport/app/build.gradle.kts
+++ GoSport/app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
 }
